58b0e0dc261b7a1d65f965061354b842614dc6d5
thebrianemory <the@brianemory.com>
2016-05-23 10:47:16 -0700

    Update carrierwave config

--- config/environment.rb
+++ config/environment.rb
@@ -23,17 +23,19 @@ configure :production do
  )
 end
 
+ActiveRecord::Base.raise_in_transactional_callbacks = true
+
 require_all 'app'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
     config.root = ApplicationController.public_folder
     config.fog_credentials = {
-    :provider              => 'AWS',                     # required
-    :aws_access_key_id     => ENV['AWS_S3_ACCESS_KEY'],  # required
-    :aws_secret_access_key => ENV['AWS_S3_KEY_ID'],      # required
-    :region                => ENV['AWS_S3_REGION']      # optional, defaults to 'us-east-1'
+    provider:              'AWS',                     # required
+    aws_access_key_id:     "#{ENV['AWS_S3_ACCESS_KEY']}",  # required
+    aws_secret_access_key: "#{ENV['AWS_S3_KEY_ID']}",      # required
+    region:                "#{ENV['AWS_S3_REGION']}",      # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['AWS_S3_BUCKET']        # required
+  config.fog_directory  = "#{ENV['AWS_S3_BUCKET'] }"       # required
   config.fog_public     = true                       # optional, defaults to true
   end
