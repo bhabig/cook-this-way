404cec2b28b77a579330d6d943e758b636b8ecca
thebrianemory <the@brianemory.com>
2016-05-24 08:17:11 -0700

    Add javascript type to layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,3 @@
-# Create users
-5.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  user = User.create(
-    provider: "facebook",
-    uid: Faker::Number.between(100000, 1000000),
-    name: first_name + " " + last_name,
-    email: Faker::Internet.safe_email(first_name + "." + last_name),
-    oauth_token: Faker::Lorem.characters(30),
-    oauth_expires_at: Faker::Time.forward(30, :morning)
-  )
-end
-# Create ingredients
-200.times do
-
-  ingredient = Ingredient.create(name: Faker::Hipster.word, measurement_type: Faker::Hacker.noun, amount: Faker::Number.between(1, 4))
-end
-
 # Create Categories
 Category.create(name: 'appetizers')
 Category.create(name: 'soups')
@@ -25,16 +6,39 @@ Category.create(name: 'entrees')
 Category.create(name: 'desserts')
 Category.create(name: 'beverages')
 
-# Create Recipes
-30.times do
-  instructions = Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3)
-  name = Faker::Beer.hop + " " + Faker::Hacker.adjective + " " + Faker::Team.creature
-  recipe = Recipe.create(name: name, instructions: instructions, user_id: Faker::Number.between(1, 6), category_id: Faker::Number.between(1, 6))
-  recipe.avatar = File.open(ApplicationController.public_folder + "/images/missing.jpg")
-  recipe.save!
 
-  5.times do
-    ingredient = Ingredient.find_by_id(Faker::Number.between(1, 200))
-    recipe.ingredients << ingredient unless recipe.ingredients.include?(ingredient)
-  end
-end
+# # Create users
+# 5.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   user = User.create(
+#     provider: "facebook",
+#     uid: Faker::Number.between(100000, 1000000),
+#     name: first_name + " " + last_name,
+#     email: Faker::Internet.safe_email(first_name + "." + last_name),
+#     oauth_token: Faker::Lorem.characters(30),
+#     oauth_expires_at: Faker::Time.forward(30, :morning)
+#   )
+# end
+
+
+# # Create ingredients
+# 200.times do
+#
+#   ingredient = Ingredient.create(name: Faker::Hipster.word, measurement_type: Faker::Hacker.noun, amount: Faker::Number.between(1, 4))
+# end
+
+
+# # Create Recipes
+# 30.times do
+#   instructions = Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3)
+#   name = Faker::Beer.hop + " " + Faker::Hacker.adjective + " " + Faker::Team.creature
+#   recipe = Recipe.create(name: name, instructions: instructions, user_id: Faker::Number.between(1, 6), category_id: Faker::Number.between(1, 6))
+#   recipe.avatar = File.open(ApplicationController.public_folder + "/images/missing.jpg")
+#   recipe.save!
+#
+#   5.times do
+#     ingredient = Ingredient.find_by_id(Faker::Number.between(1, 200))
+#     recipe.ingredients << ingredient unless recipe.ingredients.include?(ingredient)
+#   end
+# end
