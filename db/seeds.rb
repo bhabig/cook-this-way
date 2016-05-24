53c4612a25b208db7608a609d36793e4663373ea
thebrianemory <the@brianemory.com>
2016-05-24 08:46:02 -0700

    Update seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# Create Categories
-Category.create(name: 'appetizers')
-Category.create(name: 'soups')
-Category.create(name: 'salads')
-Category.create(name: 'entrees')
-Category.create(name: 'desserts')
-Category.create(name: 'beverages')
+# # Create Categories
+# Category.create(name: 'appetizers')
+# Category.create(name: 'soups')
+# Category.create(name: 'salads')
+# Category.create(name: 'entrees')
+# Category.create(name: 'desserts')
+# Category.create(name: 'beverages')
 
 
 # # Create users
@@ -22,23 +22,23 @@ Category.create(name: 'beverages')
 # end
 
 
-# # Create ingredients
-# 200.times do
-#
-#   ingredient = Ingredient.create(name: Faker::Hipster.word, measurement_type: Faker::Hacker.noun, amount: Faker::Number.between(1, 4))
-# end
+# Create ingredients
+5.times do
+
+  ingredient = Ingredient.create(name: Faker::Hipster.word, measurement_type: Faker::Hacker.noun, amount: Faker::Number.between(1, 4))
+end
 
 
-# # Create Recipes
-# 30.times do
-#   instructions = Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3)
-#   name = Faker::Beer.hop + " " + Faker::Hacker.adjective + " " + Faker::Team.creature
-#   recipe = Recipe.create(name: name, instructions: instructions, user_id: Faker::Number.between(1, 6), category_id: Faker::Number.between(1, 6))
-#   recipe.avatar = File.open(ApplicationController.public_folder + "/images/missing.jpg")
-#   recipe.save!
-#
-#   5.times do
-#     ingredient = Ingredient.find_by_id(Faker::Number.between(1, 200))
-#     recipe.ingredients << ingredient unless recipe.ingredients.include?(ingredient)
-#   end
-# end
+# Create Recipes
+1.times do
+  instructions = Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3) + "\n" + Faker::Hipster.paragraph(3)
+  name = Faker::Beer.hop + " " + Faker::Hacker.adjective + " " + Faker::Team.creature
+  recipe = Recipe.create(name: name, instructions: instructions, user_id: Faker::Number.between(1, 6), category_id: Faker::Number.between(1, 6))
+  recipe.avatar = File.open("public/images/missing.jpg")
+  recipe.save!
+
+  5.times do
+    ingredient = Ingredient.find_by_id(Faker::Number.between(1, 200))
+    recipe.ingredients << ingredient unless recipe.ingredients.include?(ingredient)
+  end
+end
